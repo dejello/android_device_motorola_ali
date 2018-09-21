866d2173c68d00d84bd012c91ce7237d84b08b79
dejello <jldegelia@gmail.com>
2018-09-21 10:05:08 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,11 +41,16 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
-
-#TARGET_2ND_ARCH := arm64
-#TARGET_2ND_ARCH_VARIANT := armv8-a
-#TARGET_2ND_CPU_ABI := arm64-v8a
-#TARGET_2ND_CPU_ABI2 :=
+# 64bit testing, comment out the above and uncomment below to try
+#TARGET_ARCH := arm64
+#TARGET_ARCH_VARIANT := armv8-a
+#TARGET_CPU_ABI := arm64-v8a
+#TARGET_CPU_ABI2 :=
+#TARGET_CPU_VARIANT := cortex-a53
+#TARGET_2ND_ARCH := arm
+#TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+#TARGET_2ND_CPU_ABI := armeabi-v7a
+#TARGET_2ND_CPU_ABI2 := armeabi
 #TARGET_2ND_CPU_VARIANT := cortex-a53
 
 #
@@ -79,7 +84,6 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 26419903488	# 25800687 * 1024 mmcblk0p53
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := ali_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237
@@ -88,7 +92,6 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_DTBTOOL_ARGS := -2
-#BOARD_DTBTOOL_ARGS := -s 2048
 TARGET_CUSTOM_DTBTOOL := dtbTool_custom # using custom for minimal twrp build
 BOARD_KERNEL_LZ4C_DT := true
 PRODUCT_VENDOR_KERNEL_HEADERS :=  $(DEVICE_PATH)/kernel-headers
