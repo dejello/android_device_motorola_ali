ce1c4a5833d8d7150a471b093b4d4de38eaf6bcb
Jonathan Degelia <jldegelia@gmail.com>
2018-09-21 08:32:27 -0500

    add 64bit arch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,11 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 
+TARGET_2ND_ARCH := arm64
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := arm64-v8a
+TARGET_2ND_CPU_ABI2 :=
+TARGET_2ND_CPU_VARIANT := cortex-a53
 #
 # Binder API version
 #
