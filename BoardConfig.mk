d763c1819928cd374461092c8195af1adb0c32b3
dejello <jldegelia@gmail.com>
2018-10-14 00:33:11 -0500

    Testing added decryption

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,97 +14,134 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/motorola/ali
 DEVICE_PATH := device/motorola/ali
 
 BOARD_VENDOR := motorola-qcom
 
-#
-# Display
-#
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 2160
+# All of the if statements are ugly....  I don't care right now.
+# Testing between 32 bit (arm) and 64 bit (arm64), added only to make changes easier
+# If arm64 is not defined it'll assume arm.
+ARCH_TYPE := arm # arm or arm64
+KERNEL_PREBUILT := true # use the prebuilt kernel and dt.img?
+
 
 #
 # Board
 #
 TARGET_BOARD_PLATFORM := msm8953
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
-TARGET_BOOTLOADER_BOARD_NAME := ali
+TARGET_BOOTLOADER_BOARD_NAME := msm8953
 TARGET_NO_BOOTLOADER := true
 
 #
 # Architecture
 #
-#TARGET_ARCH := arm
-#TARGET_ARCH_VARIANT := armv7-a-neon
-#TARGET_CPU_ABI := armeabi-v7a
-#TARGET_CPU_ABI2 := armeabi
-#TARGET_CPU_VARIANT := cortex-a53
-# 64bit testing, comment out the above and uncomment below to try
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a53
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-
-#
-# Binder API version
-#
-TARGET_USES_64_BIT_BINDER := true
+ifeq ($(ARCH_TYPE),arm64)
+     # arm64 - 64bit
+     TARGET_ARCH := arm64
+     TARGET_ARCH_VARIANT := armv8-a
+     TARGET_CPU_ABI := arm64-v8a
+     TARGET_CPU_ABI2 :=
+     TARGET_CPU_VARIANT := cortex-a53
+     TARGET_2ND_ARCH := arm
+     TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+     TARGET_2ND_CPU_ABI := armeabi-v7a
+     TARGET_2ND_CPU_ABI2 := armeabi
+     TARGET_2ND_CPU_VARIANT := cortex-a53
+     # 64bit Binder API version
+     TARGET_USES_64_BIT_BINDER := true
+else
+     # If arm64 is not defined, then assume arm - 32bit
+     TARGET_ARCH := arm
+     TARGET_ARCH_VARIANT := armv7-a-neon
+     TARGET_CPU_ABI := armeabi-v7a
+     TARGET_CPU_ABI2 := armeabi
+     TARGET_CPU_VARIANT := cortex-a53
+endif
 
 #
 # Asserts
 #
-# TARGET_OTA_ASSERT_DEVICE := ali,ali_retail
+# too much confusion on the Moto G6 forums, this is for Ali only (G6, not G6 Play or G6 Plus)
+TARGET_OTA_ASSERT_DEVICE := ali,ali_retail
 
-# Crypto
-#TARGET_HW_DISK_ENCRYPTION := true
-#TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+#
+# Encryption
+#
+BOARD_USES_QCOM_HARDWARE := true
+#TARGET_CRYPTFS_HW_PATH := $(DEVICE_PATH)/cryptfs_hw
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+TARGET_PROVIDES_KEYMASTER := true
+TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd hwservicemanager keymaster-3-0
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
 
 #
 # Partitions - pulled from ali hardware
 #
 BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_BOOTIMAGE_PARTITION_SIZE := 23068672		# 22528    * 1024 mmcblk0p36
-BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456		# 262144   * 1024 mmcblk0p51
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 23171072		# 22628    * 1024 mmcblk0p37
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3271557120		# 3194880  * 1024 mmcblk0p52
+BOARD_BOOTIMAGE_PARTITION_SIZE := 23068672		#    22528 * 1024 mmcblk0p36
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456		#   262144 * 1024 mmcblk0p51
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432		#    32768 * 1024 mmcblk0p29
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 23171072		#    22628 * 1024 mmcblk0p37
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3271557120		#  3194880 * 1024 mmcblk0p52
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 26419903488	# 25800687 * 1024 mmcblk0p53
+BOARD_VENDORIMAGE_PARTITION_SIZE := 671088640		#   655360 * 1024 mmcblk0p50
+# fs types
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 #
 # Kernel
 #
-# 32bit vs 64bit:
-#TARGET_KERNEL_ARCH := arm # 32bit
-#BOARD_KERNEL_IMAGE_NAME := zImage # 32bit
-#TARGET_KERNEL_CROSS_COMPILE_PREFIX = arm-linux-androideabi- # 32bit
-TARGET_KERNEL_ARCH := arm64 # 64bit
-BOARD_KERNEL_IMAGE_NAME := Image.gz # 64bit
-TARGET_KERNEL_CROSS_COMPILE_PREFIX = aarch64-linux-android- # 64bit
+# Testing between 32 bit (arm) and 64 bit (arm64), added only to make changes easier
+ifeq ($(ARCH_TYPE),arm64)
+     # arm64 - 64bit
+     BOARD_KERNEL_IMAGE_NAME := Image.gz
+     TARGET_KERNEL_ARCH := arm64
+     TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+else
+     # If arm64 is not defined, then assume arm - 32bit
+     BOARD_KERNEL_IMAGE_NAME := zImage
+     TARGET_KERNEL_ARCH := arm
+     TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+endif
 
-TARGET_KERNEL_CONFIG := ali_defconfig
-TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237
-BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=400M
-BOARD_KERNEL_LZ4C_DT := true
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_DTBTOOL_ARGS := -2
+ifeq ($(KERNEL_PREBUILT),true)
+     TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dt-lz4.img
+     ifeq ($(ARCH_TYPE),arm64)
+          # ARM64 - 64bit
+          TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
+     else
+          # If ARM64 is not defined, then assume ARM - 32bit
+          TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/zImage
+     endif
+endif
+
+ifneq ($(strip $(KERNEL_PREBUILT)),)
+       BOARD_DTBTOOL_ARGS := -2
+       BOARD_KERNEL_LZ4C_DT := true
+       TARGET_KERNEL_CONFIG := ali_defconfig
+       TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
+endif
+
+BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += vmalloc=400M androidboot.selinux=permissive
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+LZMA_RAMDISK_TARGETS := recovery
 PRODUCT_VENDOR_KERNEL_HEADERS :=  $(DEVICE_PATH)/kernel-headers
-TARGET_CUSTOM_DTBTOOL := dtbTool_custom # using custom for minimal twrp build
+#TARGET_CUSTOM_DTBTOOL := dtbTool_custom
 
 #
 # Recovery
 #
+TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 
@@ -112,18 +149,40 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 # Recovery - twrp
 #
 RECOVERY_VARIANT := twrp
+ALLOW_MISSING_DEPENDENCIES=true
 BOARD_HAS_NO_REAL_SDCARD := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_RECOVERY_SWIPE := true
 BOARD_USES_MMCUTILS := true
 BOARD_SUPPRESS_EMMC_WIPE := true
-TW_EXCLUDE_SUPERSU := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_INCLUDE_CRYPTO := true
-TW_DEFAULT_BRIGHTNESS := 128
-RECOVERY_SDCARD_ON_DATA := true
+#RECOVERY_SDCARD_ON_DATA := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
-ALLOW_MISSING_DEPENDENCIES=true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/etc/recovery.fstab
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/soc/7000000.ssusb/7000000.dwc3/gadget/lun0/file"
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_DEFAULT_BRIGHTNESS := 128
+TW_EXCLUDE_SUPERSU := true # TW_OEM_BUILD := true will preselect this
+#TW_EXTRA_LANGUAGES := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+#TW_OEM_BUILD := true # sets an OEM friendly version
+TW_THEME := portrait_hdpi
+TW_USE_TOOLBOX := true # TW_OEM_BUILD := true will preselect this
+TW_TARGET_USES_QCOM_BSP := true # testing
+TWHAVE_SELINUX := false # testing
+
+# TWRP Debug Flags
+#TWRP_EVENT_LOGGING := true # annoyingly log everything twrp sees......like touch events
+#TARGET_USES_LOGD := true
+#TWRP_INCLUDE_LOGCAT := true
+#TARGET_RECOVERY_DEVICE_MODULES += strace debuggerd
+#TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(TARGET_OUT_OPTIONAL_EXECUTABLES)/strace $(TARGET_OUT_EXECUTABLES)/debuggerd
+#TARGET_RECOVERY_DEVICE_MODULES += twrpdec
+#TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(TARGET_RECOVERY_ROOT_OUT)/sbin/twrpdec
+#TW_CRYPTO_SYSTEM_VOLD_DEBUG := true
+
+#Treble
+BOARD_NEEDS_VENDORIMAGE_SYMLINK := false
+TARGET_COPY_OUT_VENDOR := vendor
+BOARD_HAS_LARGE_FILESYSTEM := true
 
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # twrp_device_motorola_ali
 
 Device tree for building TWRP for the Moto G6 (ALI):
+Files have been modified from TWRPBuilder script and device tree from ADDISON---then modified much much more.
 
-Files have been modified from TWRPBuilder script and device tree from ADDISON.
+Decryption is currently in testing phase.  It works with OPSS27.104-15-10 for sure.
+ -If you want to test, please make whatever backups you think you'll want before hand.
+
+
+This should build using minimal TWRP manifest.
+
